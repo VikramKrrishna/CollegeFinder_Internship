aa3b10d21bdc02e3d5ec3fc564bb5332dbf81287
VIKRAM MEDA <88394308+VikramKrrishna@users.noreply.github.com>
2023-01-08 23:09:20 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -24,23 +24,24 @@ def mod(a,b):
     modulo = a % b
     print("The modulo of", a ,"and" ,b ,"is", modulo)
 
-#multiple inputs accepted
-num1, oper, num2 = map(str,input("Enter your Equation: ").split())
-num1 = int(num1)
-num2 = int(num2)
+while True:
+    num1, oper, num2 = map(str,input("Enter your Equation: ").split())
+    num1 = int(num1)
+    num2 = int(num2)
 
-if oper == "+":
-    add(num1,num2)
-elif oper == "-":
-     sub(num1,num2)
-elif oper == "*":
-    mul(num1,num2)
-elif oper == "/":
-    div(num1,num2)
-elif oper == "%":
-    mod(num1,num2)
-else:
-    print("***Invalid Entry***\n")
-    print("Enter valid equation like 22 + 23, 15 * 22, 7 - 3")
+    if oper == "+":
+        add(num1,num2)
+    elif oper == "-":
+         sub(num1,num2)
+    elif oper == "*":
+        mul(num1,num2)
+    elif oper == "/":
+        div(num1,num2)
+    elif oper == "%":
+        mod(num1,num2)
+    else:
+        print("***Invalid Entry***\n")
+        print("Enter valid equation like 22 + 23, 15 * 22, 7 - 3")
+    print("-------------------------------------------")
 
 
